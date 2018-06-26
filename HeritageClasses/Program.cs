dca731d632c9b5875fdc121abe0873ca11113be6
Laetitia FOUCHERE <laetitia.fouchere.lp2@gmail.com>
2018-06-26 12:08:13 +0200

    first commit

---
+++ HeritageClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
